b3c3247e5872067f02d8e269b7c21f260635df05
Daler <daler1912@gmail.com>
2019-10-02 16:52:52 +0600

    Added regularization

--- Week3/Ex2data2.py
+++ Week3/Ex2data2.py
@@ -9,18 +9,11 @@ def isclose(a, b, rtol=1e-05, atol=1e-08):
 def sigma(z):
 	return 1/(1 + np.exp(-z))
 
-# m = 100
-# X1 = np.random.randint(1000, size=m).astype(dtype=np.float32)
-# X2 = np.random.randint(1000, size=m).astype(dtype=np.float32)
-# Y = np.array([1 if x2 > f(x1) else 0
-# 			for x1, x2 in zip(X1, X2)], dtype=np.float32)
-# for i in range(m):
-# 	if random.random() < 0.03:
-# 		Y[i] = 1 - Y[i] 
-data = pandas.read_csv('ex2data1.txt')
+data = pandas.read_csv('ex2data2.txt')
 data = data.to_numpy()
 
 y = [data[i][2] for i in range(len(data))]
+y = np.array(y)
 x = np.delete(data, 2, 1)
 
 # y = [(i-np.mean(y))/(np.max(y)-np.min(y)) for i in y]
@@ -28,11 +21,20 @@ x = np.delete(data, 2, 1)
 # x = np.array([[(x[i][j]-np.mean(x[:, j]))/(np.max(x[:, j])-np.min(x[:, j])) for j in range(len(x[i]))] for i in range(len(x))])
 
 x = np.array([np.insert(i, 0, 1) for i in x])
+m = len(x)
 
+def mapFeature(X1, X2):
+    degree = 6
+    out = np.ones(len(X1))[:,np.newaxis]
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            out = np.hstack((out, np.multiply(np.power(X1, i -j), np.power(X2, j))[:,np.newaxis]))    
+    return out
 
-# print(y)
+# print(np.shape(mapFeature(x[:, 1], x[:, 2])))
 # exit(0)
-theta = np.array([0., 0., 0.])
+x = mapFeature(x[:, 1], x[:, 2])
+theta = np.zeros(28)
 lr = 1.
 m = len(x)
 dw_s=0
@@ -44,31 +46,35 @@ for _ in range(100000):
 	dw_s = dw_s*0.9 + 0.1*dw
 	theta -= lr*dw_s
 
-print(theta)
-
-aa = -theta[1]/theta[2]
-bb = -theta[0]/theta[2]
-
-def g(x):
-	return aa*x + bb
-
-print(y)
+# print(theta)
 
 yy = np.ones(m)
 red = np.where(isclose(y, yy))
 yy = np.zeros(m)
 blue = np.where(isclose(y, yy))
 
-print(x)
-
 plt.plot(x[red, 1], x[red, 2], 'ro')
 plt.plot(x[blue, 1], x[blue, 2], 'bo')
-# for i in range(len(x)):
 
-    
+u = np.linspace(-1, 1.1, 50)
+v = np.linspace(-1, 1.1, 50)
+z = np.zeros((len(u), len(v)))
 
-_x = [np.amin(x[:, 1]), np.amax(x[:, 2])]
-_y = [g(i) for i in _x]
-plt.plot(_x, _y)
+def mapFeatureForPlotting(X1, X2):
+    degree = 6
+    out = np.ones(1)
+    for i in range(1, degree+1):
+        for j in range(i+1):
+            out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))))
+    return out
 
+for i in range(len(u)):
+    for j in range(len(v)):
+        z[i,j] = np.dot(mapFeatureForPlotting(u[i], v[j]), theta)
+
+pred = [sigma(np.dot(x, theta)) >= 0.5]
+
+print(np.mean(pred == y.flatten()) * 100)
+plt.contour(u,v,z,0)
 plt.show()
+
