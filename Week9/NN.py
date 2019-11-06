ac07bdab0ca767fa108d9eae8211cf9baea24c65
Romulus <daler1912@gmail.com>
2019-11-06 13:51:04 +0600

    WIP Neural net

--- Week9/NN.py
+++ Week9/NN.py
@@ -6,25 +6,59 @@ import pandas
 import cv2
 import tkinter
 from copy import deepcopy
+from scipy.special import expit
+from scipy.special import softmax as soft
 
 # matplotlib.use('TkAgg')
+def sigma(z):
+    return 1.0/(1.0 + np.exp(-z))
+def ReLU(z):
+    return z
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()  
+np.random.seed(42)
+lr = 0.1
+class NeuralNetwork:
+    def __init__(self, x, y, hidden_layer_size):
+        self.input      = x
+        self.weights1   = np.random.rand(self.input.shape[1], hidden_layer_size) * 0.1
+        self.weights2   = np.random.rand(hidden_layer_size, 7) * 0.1
+        self.y          = y
+        self.output     = np.zeros(y.shape)
+
+    
+
+      
+    def forward_propagation(self):
+        self.layer1 = sigma(np.dot(self.input, self.weights1))
+        self.output = sigma(np.dot(self.layer1, self.weights2))
+        # print(np.dot(self.input, self.weights1))
+        # print(np.exp(-np.dot(self.input, self.weights1)))
+        # exit(0)
+        # print(self.layer1)
+        # print(self.output)
+        # print(soft(self.output))
+        # exit(0)
+        return softmax(self.output)
+        return self.output
+    
+    def backprop(self):
+        d_weights2 = np.dot(self.layer1.T, 2 * (self.y -self.output) * self.output * (1 - self.output))
+        d_weights1 = np.dot(self.input.T, np.dot(2 * (self.y -self.output) * self.output * (1 - self.output), self.weights2.T) * self.layer1 * (1 - self.layer1))
+        # print(self.y)
+        # print(self.output)
+        self.weights1 += d_weights1 * lr
+        self.weights2 += d_weights2 * lr
+        # exit(0)
+    
+    def train(self, X, y):
+        self.output = self.forward_propagation()
+        self.backprop()
 
 def isclose(a, b, rtol=1e-05, atol=1e-08):
     return np.abs(a-b) <= (atol + rtol * np.abs(b))
-
-def sigma(z):
-	return 1/(1 + np.exp(-z))
-
-def ReLU(z) {
-    return z
-}
-def cost_function(X, y, thetas):
-    h = 1/(1 + np.exp(-X.dot(thetas)))
-    cost_1 = np.log(h)
-    cost_2 = np.array([np.log(1-i) for i in h])
-    summ = y.dot(cost_1) + np.array([1-i for i in y]).dot(cost_2)
-    return -summ/m
-
+    
 data = pandas.read_csv('glass.csv')
 
 data = data.sample(frac=1).reset_index(drop=True)
@@ -35,80 +69,26 @@ data = data.drop('Type', axis = 1)
 data = data.to_numpy()
 y_data = y_data.to_numpy()
 
-x = data
+
+one_hot_labels = np.zeros((len(y_data), 7))
+y_data -= 1
+for i in range(len(y_data)):
+    one_hot_labels[i, y_data[i]] = 1
+NN = NeuralNetwork(data, one_hot_labels, 10)
+for i in range(10): 
+    print ("for iteration # " + str(i) + "\n")
+    NN.train(data, one_hot_labels)
+# print(NN.weights1)
+# print(NN.weights2)
+res = NN.forward_propagation()
+res = np.array(res).astype(dtype=float)
+# print(res)
+# exit(0)
+
+print ("Input : \n" + str(data))
+print ("Actual Output: \n" + str(y_data))
+print ("Predicted Output: \n" + str(NN.forward_propagation()))
 
 # y = [(i-np.mean(y))/(np.max(y)-np.min(y)) for i in y]
 
 # x = np.array([[(x[i][j]-np.mean(x[:, j]))/(np.max(x[:, j])-np.min(x[:, j])) for j in range(len(x[i]))] for i in range(len(x))])
-
-y = deepcopy(y_data)
-
-x = np.array([np.insert(i, 0, 1) for i in x])
-m = len(x)
-
-# theta = np.ndarray(shape=(7, 10))
-# theta.fill(0)
-theta = np.random.randn(7, 10) * 0.001
-
-lr = 0.001
-alpha = 0.7
-dw_s=0
-
-costs = np.zeros((7, 10000))
-
-for type in range(7):  
-        y = deepcopy(y_data)  
-        idxs_i = y == type + 1
-        y[idxs_i] = 1
-        y[~idxs_i] = 0
-        for i in range(10000):
-                Z = x.dot(theta[type])
-                a = sigma(Z)
-                dz = a - y
-                dw = x.T.dot(dz)/m
-                dw += (np.sum(theta[type]) - theta[type][0]) / m * alpha
-                # dw_s = dw_s * 0.9 + 0.1 * dw
-                theta[type] -= lr * dw
-                costs[type][i] = cost_function(x, y, theta[type])
-# costs.append()
-
-
-for type in range(7):
-        print(theta[type])
-        plt.plot(range(len(costs[type])), costs[type])
-        # plt.axis([xmin, xmax, ymin, ymax])
-        plt.title("The cost function plot of glass of the following type " + str(type))
-        plt.show()
-        # input("")
-        # exit(0);
-
-
-rres = 0.0
-for type in range(7):
-    for i in range(len(y_data)):        
-        y[i] = 1 if y_data[i] == (type + 1) else 0
-    # print(y)
-    pred = [sigma(np.dot(x, theta[type])) >= 0.5]
-    print(np.mean(pred == y.flatten()) * 100)
-    rres += np.mean(pred == y.flatten()) * 100
-
-
-# exit(0)
-
-print("")
-
-result = 0.0
-for i in range(len(x)):
-    res = []
-    for type in range(7):
-        res.append(sigma(np.dot(x[i], theta[type])))        
-#     print(res)
-#     print (np.argmax(res)+1)
-#     print (y_data[i])
-    result += np.argmax(res) + 1 == y_data[i]
-
-print (rres / 7)
-    
-print (result / m * 100) 
-
-
